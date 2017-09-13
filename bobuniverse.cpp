0617b47313542cdf3bd8f08d3d752051b808a952
otacilioneto <otacilio.n97@gmail.com>
2017-09-13 04:10:13 +0000

    Adding basic structure

---
+++ bobuniverse.cpp
@@ -0,0 +1,12 @@
+/*
+ * bobuniverse.cpp
+ * 
+ * It is a Bob Robot Universe Simulator
+ *
+ * Created on September 13, 2017
+ * By Otacilio Neto (github: @OtacilioN | Email: contato@otaciliomaia.com)
+ * 
+ * Published under mit license
+ */
+ 
+#include "Brasilino.h"
---
+++ bobuniverse.h
@@ -0,0 +1,15 @@
+/*
+ * bobuniverse.h
+ * 
+ * It is a Bob Robot Universe Simulator
+ *
+ * Created on September 13, 2017
+ * By Otacilio Neto (github: @OtacilioN | Email: contato@otaciliomaia.com)
+ * 
+ * Published under mit license
+ */
+ 
+#ifndef bobuniverse_h
+#define bobuniverse_h
+
+#endif
